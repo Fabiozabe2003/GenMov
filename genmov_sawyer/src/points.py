e2d32c7cdb356fd0889f334db078e17821400225
Andres <andres.obludzyner@gmail.com>
2024-06-13 10:19:12 -0500

    Added multiple points

---
+++ genmov_sawyer/src/points.py
@@ -0,0 +1,6 @@
+import numpy as np
+from lab4functions import *
+
+# Load the array from the txt file
+points = np.loadtxt('/home/andres/momask-codes/right_hand_points.txt', dtype=float)
+print(points[0])
